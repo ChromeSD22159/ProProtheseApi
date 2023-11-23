243424472e6a291754dc62658afac561083fe13e
ChromeSD22159 <65664594+ChromeSD22159@users.noreply.github.com>
2023-11-23 02:38:11 +0100

    fix

--- frontend/server/api/betteruptime.js
+++ frontend/server/api/betteruptime.js
@@ -1,11 +1,10 @@
 export default defineEventHandler(async (event) => {
+    
     const config = useRuntimeConfig(event)
-
-    //const betterUptimePages = JSON.parse(config.BETTERUPTIMEPAGES);
-
     return await $fetch('https://betteruptime.com/api/v2/status-pages/174778', {
         headers: {
             Authorization: `Bearer ${config.betterUptimeToken}`
         },
-    }
+    })
+
 })
